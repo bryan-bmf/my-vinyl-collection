aa9d56d7663c5f6fd151c1a9bbe4520eb44867c3
Bryan Mitchell <bryan.bmf@gmail.com>
2024-01-04 10:22:19 -0500

    Missing components

---
+++ src/components/PlayerModal.tsx
@@ -0,0 +1,57 @@
+import {
+	Box,
+	Button,
+	Center,
+	Modal,
+	ModalBody,
+	ModalCloseButton,
+	ModalContent,
+	ModalFooter,
+	ModalHeader,
+	ModalOverlay,
+	Spinner,
+	useDisclosure,
+} from "@chakra-ui/react";
+import SpotifyPlayer from "./SpotifyPlayer";
+import { useEffect, useState } from "react";
+
+const PlayerModal = (props: any) => {
+	const [loading, setLoading] = useState<Boolean>(true);
+
+	const { isOpen, onClose } = props.modalDisclosure;
+
+	//show loading spinner for a second
+	useEffect(() => {
+		setLoading(true)
+		const timer = setTimeout(() => {
+			setLoading(false);
+		}, 1000);
+		return () => clearTimeout(timer);
+	}, [props]);
+
+	return (
+		<Modal isOpen={isOpen} onClose={onClose} isCentered>
+			<ModalOverlay />
+			{loading ? (
+				<Center>
+					<Spinner size="xl" />
+				</Center>
+			) : (
+				<ModalContent sx={sx.modal}>
+					<SpotifyPlayer album={props.album} />
+				</ModalContent>
+			)}
+		</Modal>
+	);
+};
+
+const sx = {
+	modal: {
+		borderRadius: "10%",
+		backgroundColor: "transparent",
+		borderColor: "transparent",
+		shadow: "none",
+	},
+};
+
+export default PlayerModal;
---
+++ src/components/SpotifyPlayer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Spotify } from "react-spotify-embed";
+import { AnyObject } from "../types";
+
+const SpotifyPlayer = (props: AnyObject) => {
+	const { isAlbum, spotifyAlbumId } = props.album;
+	const type = isAlbum === true ? "album" : "playlist";
+
+	const url = "https://open.spotify.com/" + type + "/" + spotifyAlbumId;
+
+	return <Spotify width="450em" height="550em" link={url} />;
+};
+
+export default SpotifyPlayer;

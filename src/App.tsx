641ccd048e48241293323981c1b7814e891c75f5
bryan-bmf <111380447+bryan-bmf@users.noreply.github.com>
2024-01-08 14:01:19 -0500

    Chart library added with working example (#5)

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import './App.css';
 import ArtistTable from '../src/components/ArtistTable';
 import GridView from './components/GridView';
 import AlbumCover from './components/AlbumCover';
+import Charts from './components/Charts';
 
 function App() {
 
   return (
-    <div className="App">
-      <GridView />
+    <div className="App" style={{height:400}}>
+      <Charts />
     </div>
   );
 }

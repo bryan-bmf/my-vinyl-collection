f8f7a4a826614eae3a00b919c345f72c2110a9a0
Bryan Mitchell <bryan.bmf@gmail.com>
2024-01-09 14:35:19 -0500

    tidy up

--- server/aws.js
+++ server/aws.js
@@ -1,6 +1,6 @@
 // Load the AWS SDK for Node.js.
 var AWS = require("aws-sdk");
-const fs = require('fs');
+
 // Set the AWS Region.
 AWS.config.update({ region: "us-east-2" });
 
@@ -8,16 +8,6 @@ AWS.config.update({ region: "us-east-2" });
 var ddb = new AWS.DynamoDB({ apiVersion: "2012-08-10" });
 
 const params = {
-	// Specify which items in the results are returned.
-	//   FilterExpression: "Subtitle = :topic AND Season = :s AND Episode = :e",
-	//   // Define the expression attribute value, which are substitutes for the values you want to compare.
-	//   ExpressionAttributeValues: {
-	//     ":topic": {S: "SubTitle2"},
-	//     ":s": {N: 1},
-	//     ":e": {N: 2},
-	//   },
-	// Set the projection expression, which are the attributes that you want.
-	//   ProjectionExpression: "Season, Episode, Title, Subtitle",
 	TableName: "vinyls",
 };
 
@@ -34,10 +24,5 @@ ddb.scan(params, function (err, data) {
 			let unmarshalled = AWS.DynamoDB.Converter.unmarshall(element)
 			res.push(unmarshalled)
 		});
-
-		fs.writeFile('Output.txt', JSON.stringify(res), (err) => {
- 
-			if (err) throw err;
-		})
 	}
 });

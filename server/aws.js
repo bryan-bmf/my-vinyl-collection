116a5dda8e354657c7edf487478e566c76231f69
Bryan Mitchell <bryan.bmf@gmail.com>
2024-01-09 11:34:13 -0500

    Read dynamo table from node

---
+++ server/aws.js
@@ -0,0 +1,40 @@
+// Load the AWS SDK for Node.js.
+var AWS = require("aws-sdk");
+const fs = require('fs');
+// Set the AWS Region.
+AWS.config.update({ region: "us-east-2" });
+
+// Create DynamoDB service object.
+var ddb = new AWS.DynamoDB({ apiVersion: "2012-08-10" });
+
+const params = {
+	// Specify which items in the results are returned.
+	//   FilterExpression: "Subtitle = :topic AND Season = :s AND Episode = :e",
+	//   // Define the expression attribute value, which are substitutes for the values you want to compare.
+	//   ExpressionAttributeValues: {
+	//     ":topic": {S: "SubTitle2"},
+	//     ":s": {N: 1},
+	//     ":e": {N: 2},
+	//   },
+	// Set the projection expression, which are the attributes that you want.
+	//   ProjectionExpression: "Season, Episode, Title, Subtitle",
+	TableName: "vinyls",
+};
+
+let res = [];
+
+ddb.scan(params, function (err, data) {
+	if (err) {
+		console.log("Error", err);
+	} else {
+		console.log("Success", data);
+		data.Items.forEach(function (element, index, array) {
+			console.log(element);
+			res.push(element)
+		});
+		fs.writeFile('Output.txt', JSON.stringify(res), (err) => {
+ 
+			if (err) throw err;
+		})
+	}
+});

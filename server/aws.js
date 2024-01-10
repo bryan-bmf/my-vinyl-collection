4e936e05db55208f54e8f958a83569b7a8e96f20
Bryan Mitchell <bryan.bmf@gmail.com>
2024-01-09 20:58:14 -0500

    Switch ddb scan to docClient

--- server/aws.js
+++ server/aws.js
@@ -4,36 +4,18 @@ var AWS = require("aws-sdk");
 // Set the AWS Region.
 AWS.config.update({ region: "us-east-2" });
 
-// Create DynamoDB service object.
-var ddb = new AWS.DynamoDB({ apiVersion: "2012-08-10" });
+// dynamo object
+var docClient = new AWS.DynamoDB.DocumentClient({
+	apiVersion: "2012-08-10",
+});
 
 const getVinyls = async () => {
 	const params = {
 		TableName: "vinyls",
 	};
-
-	let res = [];
-
-	const data = await ddb
-		.scan(params, function (err, data) {
-			if (err) {
-				console.log("Error", err);
-			} else {
-				console.log("Success", data);
-				data.Items.map(function (element, index, array) {
-					// convert dynamo json to regular json
-					let unmarshalled = AWS.DynamoDB.Converter.unmarshall(element);
-					res.push(unmarshalled);
-				});
-			}
-		})
-		.promise();
-
-	// delete dynamo json
-	delete data.Items;
-	// add regular json
-	Object.assign(data, { items: res });
-	return data;
+	
+	const result = await docClient.scan(params).promise();
+	return result;
 };
 
 module.exports = { getVinyls };

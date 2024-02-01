8a42ce5f74ad2a02a8476d5f618c7a43af878a9a
Bryan Mitchell <bryan.bmf@gmail.com>
2024-02-01 15:48:39 -0500

    more webpack stuff

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,15 +1,20 @@
 const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-	entry: path.resolve(__dirname, "./public/index.html"),
+	entry: path.resolve(__dirname, "./src/index.tsx"),
 	output: {
 		path: path.resolve(__dirname, "dist"),
 		filename: "bundle.js",
 		publicPath: "/",
 	},
+	devServer: {
+        historyApiFallback: true
+     },
 	plugins: [
-		new HtmlWebpackPlugin({
-			template: path.resolve(__dirname, "src", "./public/index.html"),
+		new HtmlWebPackPlugin({
+			template: path.resolve(__dirname, "./public/index.html"),
+			filename: "index.html"
 		}),
 	],
 };

f1e6db99ecd21d33e55f35ef54678836b9562cd0
bryan-bmf <111380447+bryan-bmf@users.noreply.github.com>
2024-02-01 08:41:53 -0500

    New scripts (#23)
    * changed favicon, etc
    
    * cambiando install scripts
    
    * run
    
    * adding meta tag
    
    * added new env variables for the api url. removed server csp stuff
    
    * adding aws creds to env file
    
    * aws secret variable name change
    
    * adding meta tag for https
    
    * meta tag
    
    * remove logo.png
    
    * added cors policy

--- server/index.js
+++ server/index.js
@@ -6,6 +6,16 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+// Add Access Control Allow Origin headers
+app.use((req, res, next) => {
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept"
+	);
+	next();
+});
+
 app.get("/vinyls", async (req, res) => {
 	const data = await aws.getVinyls();
 	res.json(data);

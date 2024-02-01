85360f4cdd13d324e096b4a4fe88715dbd2891c0
bryan-bmf <111380447+bryan-bmf@users.noreply.github.com>
2024-02-01 13:35:14 -0500

    New scripts (#25)
    * changed favicon, etc
    
    * cambiando install scripts
    
    * run
    
    * adding meta tag
    
    * added new env variables for the api url. removed server csp stuff
    
    * adding aws creds to env file
    
    * aws secret variable name change
    
    * adding meta tag for https
    
    * meta tag
    
    * remove logo.png
    
    * added cors policy
    
    * default route in server
    
    * changed path of default route
    
    * changes to default route

--- server/index.js
+++ server/index.js
@@ -26,8 +26,8 @@ app.get("/aggregate", async (req, res) => {
 	res.json(data);
 });
 
-app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "public", "index.html"));
+app.get("/*", (req, res) => {
+	res.sendFile(path.join(__dirname, "../client/public/index.html"));
 });
 
 app.listen(PORT, () => {

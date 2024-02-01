1ed28c74b5f469bfe6ff39b605f3d42b8829c2f5
bryan-bmf <111380447+bryan-bmf@users.noreply.github.com>
2024-02-01 15:27:41 -0500

    New scripts (#29)
    * changed favicon, etc
    
    * cambiando install scripts
    
    * run
    
    * adding meta tag
    
    * added new env variables for the api url. removed server csp stuff
    
    * adding aws creds to env file
    
    * aws secret variable name change
    
    * adding meta tag for https
    
    * meta tag
    
    * remove logo.png
    
    * added cors policy
    
    * default route in server
    
    * changed path of default route
    
    * changes to default route
    
    * adding path library
    
    * adding webpack config file
    
    * forgot to build
    
    * adding webpack plugin

--- client/src/webpack.config.js
+++ client/src/webpack.config.js
@@ -1,10 +1,16 @@
-const path = require('path')
+const path = require("path");
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src', 'index.tsx'),
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: "/"
-  }
-}
+	entry: path.resolve(__dirname, "src", "index.tsx"),
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "bundle.js",
+		publicPath: "/",
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, "src", "index.tsx"),
+            filename: "index.html",
+		}),
+	],
+};

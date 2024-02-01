496d03379600579bbaa1f32110eba2cf1840c284
Bryan Mitchell <bryan.bmf@gmail.com>
2024-02-01 13:23:43 -0500

    changed path of default route

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ app.get("/aggregate", async (req, res) => {
 });
 
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "public", "index.html"));
+	res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 app.listen(PORT, () => {

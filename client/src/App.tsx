0d1799add7370084113288b9d5e2f8c58db92d7e
bryan-bmf <111380447+bryan-bmf@users.noreply.github.com>
2024-02-01 20:41:24 -0500

    New scripts (#33)
    * changed favicon, etc
    
    * cambiando install scripts
    
    * run
    
    * adding meta tag
    
    * added new env variables for the api url. removed server csp stuff
    
    * adding aws creds to env file
    
    * aws secret variable name change
    
    * adding meta tag for https
    
    * meta tag
    
    * remove logo.png
    
    * added cors policy
    
    * default route in server
    
    * changed path of default route
    
    * changes to default route
    
    * adding path library
    
    * adding webpack config file
    
    * forgot to build
    
    * adding webpack plugin
    
    * change index route
    
    * more webpack stuff
    
    * deleting webpack. trying new node way
    
    * using hashrouter
    
    * changing to build folder

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import { AnimatePresence } from "framer-motion";
-import { HashRouter, Route, useLocation } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Collection from "./pages/Collection";
 import Home from "./pages/Home";
@@ -8,16 +8,14 @@ import NotFound from "./pages/NotFound";
 export const URL = process.env.REACT_APP_SERVER_URL;
 
 function App() {
-	const location = useLocation();
-
 	return (
 		<div className="App">
 			<AnimatePresence>
-				<HashRouter>
+				<Routes>
 					<Route path="/" element={<Home />} />
 					<Route path="/collection" element={<Collection />} />
-					<Route path="/*" element={<NotFound />} />
-				</HashRouter>
+					<Route path="*" element={<NotFound />} />
+				</Routes>
 			</AnimatePresence>
 		</div>
 	);
--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ app.get("/aggregate", async (req, res) => {
 });
 
 app.get('/*', function(req, res) {
-	res.sendFile(path.join(__dirname, '../client/public/index.html'), function(err) {
+	res.sendFile(path.join(__dirname, '../client/build/index.html'), function(err) {
 	  if (err) {
 		res.status(500).send(err)
 	  }

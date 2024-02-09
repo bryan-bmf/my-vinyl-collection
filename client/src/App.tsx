27266a25ec4b46c9ba6c2d413a449ffc8ff49a60
Bryan Mitchell <bryan.bmf@gmail.com>
2024-02-08 21:29:35 -0500

    page setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import { AnimatePresence } from "framer-motion";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import AddVinyl from "./pages/AddVinyl";
 import Collection from "./pages/Collection";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
@@ -14,6 +15,7 @@ function App() {
 				<Routes>
 					<Route path="/" element={<Home />} />
 					<Route path="/collection" element={<Collection />} />
+					<Route path="/add" element={<AddVinyl />} />
 					<Route path="*" element={<NotFound />} />
 				</Routes>
 			</AnimatePresence>
